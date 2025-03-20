4b34b0e5a1ba7955a0436719cec5da6a833b141c
Vortex Agencia <98716392+Appvortex@users.noreply.github.com>
2025-03-20 09:07:13 -0500

    Update wbot.ts

--- backend/src/libs/wbot.ts
+++ backend/src/libs/wbot.ts
@@ -13,7 +13,7 @@ import makeWASocket, {
   makeCacheableSignalKeyStore,
   makeInMemoryStore,
 } from "@whiskeysockets/baileys";
-import makeInMemoryStore from '@whiskeysockets/baileys';
+
 import { FindOptions } from "sequelize/types";
 import Whatsapp from "../models/Whatsapp";
 import logger from "../utils/logger";
